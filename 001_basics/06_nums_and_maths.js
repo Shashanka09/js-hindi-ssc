c53319cd27381512d4cf8e6fe4744ffe9a4e99b2
Shashanka09 <139042957+Shashanka09@users.noreply.github.com>
2024-01-15 07:25:22 +0000

    math and num

---
+++ 001_basics/06_nums_and_maths.js
@@ -0,0 +1,19 @@
+const score = 400
+console.log(score)
+
+const balance = new Number(100)
+console.log(balance)
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const othernumber = 655.195481267
+console.log(othernumber.toPrecision(5));
+
+const numboor = 1000000000000
+console.log(numboor.toLocaleString('en-IN'));
+
+
+console.log(Math.floor(Math.random()*10)+1)
+max =20
+min =10
+console.log(Math.floor(Math.random()*(max-min+1))+min)
